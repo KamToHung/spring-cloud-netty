137b982813d09124b991a6a3dc1ddeb7842acad4
KamTo Hung <kamtohung@gmail.com>
2023-04-20 18:17:09 +0800

    first commit

---
+++ src/test/java/com/netty/springcloudnetty/SpringCloudNettyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netty.springcloudnetty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudNettyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
